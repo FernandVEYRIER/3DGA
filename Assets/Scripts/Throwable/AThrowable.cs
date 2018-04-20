b73ef4b580bcf7b4f67d564fcb19088fdb3ba4c4
Fernand <fernand.veyrier@epitech.eu>
2018-04-20 09:50:50 +0200

    Adde parabola throwing for AThrowable objects

--- Assets/Scripts/Throwable/AThrowable.cs
+++ Assets/Scripts/Throwable/AThrowable.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Assets.Scripts.Throwable
@@ -29,7 +30,11 @@ namespace Assets.Scripts.Throwable
             if (_isGrabbed)
             {
                 _throwForce = ComputeThrowingForce();
+                transform.position = _attachedTransform.position;
             }
+
+            if (Input.GetKeyDown(KeyCode.N))
+                Throw(Vector3.zero);
         }
 
         /// <summary>
@@ -63,13 +68,48 @@ namespace Assets.Scripts.Throwable
         protected abstract void OnObjectDestroy();
 
         /// <summary>
-        /// Throws the object.
+        /// Throws the object, with its current force.
         /// </summary>
         public virtual void Throw()
         {
             _isGrabbed = false;
             _rigidBody.AddForce(_throwForce);
             _attachedTransform = null;
+        }
+
+        /// <summary>
+        /// Throws the object with a parabola trajectory,
+        /// aiming to the given target.
+        /// </summary>
+        /// <param name="target"></param>
+        public virtual void Throw(Vector3 target)
+        {
+            _isGrabbed = false;
+            _attachedTransform = null;
+            float firingAngle = 45f, gravity = Mathf.Abs(Physics.gravity.y);
+
+            // Calculate distance to target
+            float target_Distance = Vector3.Distance(transform.position, target);
+            // Calculate the velocity needed to throw the object to the target at specified angle.
+            float projectile_Velocity = target_Distance / (Mathf.Sin(2 * firingAngle * Mathf.Deg2Rad) / gravity);
+
+            // Extract the X  Y componenent of the velocity
+            float Vx = Mathf.Sqrt(projectile_Velocity) * Mathf.Cos(firingAngle * Mathf.Deg2Rad);
+            float Vy = Mathf.Sqrt(projectile_Velocity) * Mathf.Sin(firingAngle * Mathf.Deg2Rad);
+
+            // Calculate flight time.
+            float flightDuration = target_Distance / Vx;
+
+            // Rotate projectile to face the target.
+            transform.rotation = Quaternion.LookRotation(target - transform.position);
+
+            var fwd = transform.forward;
+            fwd.x = 0;
+            fwd.y = Vy;
+            fwd.z = Vx;
+            fwd = transform.TransformDirection(fwd);
+
+            _rigidBody.velocity = fwd;
         }
 
         /// <summary>
