9a6b463548c28634508c9061ebf095056f45d270
Fernand <fernand.veyrier@epitech.eu>
2018-04-21 05:50:55 +0200

    Added liquid flow according to bottle rotation

--- Assets/Scripts/Liquids/ParticleHandler.cs
+++ Assets/Scripts/Liquids/ParticleHandler.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System;
+using Assets.Scripts.Effects;
+using UnityEngine;
 
 namespace Assets.Scripts.Liquids
 {
@@ -9,6 +11,18 @@ namespace Assets.Scripts.Liquids
     /// </summary>
     public class ParticleHandler : MonoBehaviour
     {
+        [Tooltip("The transform to watch for rotations")]
+        [SerializeField] private Transform relativeTransform;
+
+        private ParticleSystem _particles;
+        [SerializeField]
+        private float maxEmission = 40;
+
+        private void Awake()
+        {
+            _particles = GetComponent<ParticleSystem>();
+        }
+
         /// <summary>
         /// Invoked when particles collide with something.
         /// </summary>
@@ -18,6 +32,23 @@ namespace Assets.Scripts.Liquids
         {
             if (OnParticleCollided != null)
                 OnParticleCollided.Invoke(this, other);
+        }
+
+        private void Update()
+        {
+            var emi = _particles.emission;
+            float amount = ComputeParticleEmission();
+            emi.rateOverTime = new ParticleSystem.MinMaxCurve(amount);
+        }
+
+        /// <summary>
+        /// Computes the particle count according to the current angle of the Relative Transform set.
+        /// </summary>
+        /// <returns></returns>
+        private float ComputeParticleEmission()
+        {
+            var angle = Vector3.Dot(transform.forward, Vector3.down);
+            return angle * maxEmission;
         }
     }
 }
