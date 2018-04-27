2b5627fb025a9dd2b8e2bd387a842e61aa32daf1
miestl <loic.miest@epitech.eu>
2018-04-27 14:35:20 +0900

    add Vr scene and fiona's assets

---
+++ Assets/Script/SpawnManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour {
+
+	public GameObject GameManager;
+
+	public GameObject[] nbSpawner;
+
+	public GameObject enemyBehaviour;
+
+	public bool[] enemyHere;
+	// Use this for initialization
+	void Start () {
+		enemyHere = new bool[nbSpawner.Length];
+
+		for (int i = 0; i < enemyHere.Length; i++) {
+			enemyHere [i] = false;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		for (int i = 0; i < enemyHere.Length; i++) {
+			if (!enemyHere[i]) {
+				enemyHere[i] = true;
+				spawnFoe (i);
+			}
+		}
+
+	}
+
+	void spawnFoe(int wichSpawner)
+	{
+		var tmp = Instantiate (enemyBehaviour, new Vector3 (
+			nbSpawner [wichSpawner].transform.position.x,
+			nbSpawner [wichSpawner].transform.position.y + 1,
+			nbSpawner [wichSpawner].transform.position.z),
+			Quaternion.identity);
+		tmp.GetComponent<EnemyMove> ().indexEnemy = wichSpawner;
+	}
+}
---
+++ Assets/Scripts/EnemyMove.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMove : MonoBehaviour {
+
+	public int indexEnemy;
+
+	private bool goStrait;
+	private bool goBack;
+
+	// Use this for initialization
+	void Start () {
+		goStrait = true;
+		goBack = false;
+	}
+	
+	// Update is called once per frame
+	/*void Update () {
+		if (goStrait) {
+			Vector3 tmp = this.transform.position;
+			tmp.z += 0.05f;
+			this.transform.position = tmp;
+			if (this.transform.position.z >= 5) {
+				goStrait = false;
+				goBack = true;
+			}
+		}
+		else if (goBack) {
+			Vector3 tmp = this.transform.position;
+			tmp.z -= 0.05f;
+			this.transform.position = tmp;
+			if (this.transform.position.z <= -9) {
+				goStrait = true;
+				goBack = false;
+			}
+		}
+	}*/
+
+	void OnCollisionEnter(Collision coll)
+	{
+		if (coll.transform.tag == "Bottle") {
+			//GameObject.Find ("GameManager").GetComponent<SpawnManager> ().enemyHere [indexEnemy] = false;
+			Destroy (this.gameObject);
+		}
+	}
+}
