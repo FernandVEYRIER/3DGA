3a24b1ad5ad959cbc4af87c5e0f3284eea3eac10
gandoulf <enzo.resse@epitech.eu>
2018-04-27 08:18:44 +0200

    spawn sxript

---
+++ Assets/Scripts/Spawn/SpawnBottle.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnBottle : MonoBehaviour {
+
+    [SerializeField]
+    GameObject[] spawn;
+    [SerializeField]
+    GameObject bottle;
+
+    GameObject[] instantiated;
+
+    // Use this for initialization
+    private void Start()
+    {
+        instantiated = new GameObject[spawn.Length];
+    }
+
+    // Update is called once per frame
+    void Update () {
+        for (int i = 0; i < spawn.Length; i++)
+        {
+            if (instantiated[i] == null)
+                instantiated[i] = Instantiate(bottle, spawn[i].transform.position, Quaternion.identity);
+        }
+	}
+}
