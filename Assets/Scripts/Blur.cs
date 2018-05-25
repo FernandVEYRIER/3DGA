a425c8e9d1b19045967b8e288d5e70ed73a91f97
Unknown <matthieu.tavernier@epitech.eu>
2018-05-25 13:19:40 +0900

    Auto stash before merge of "develop" and "origin/develop"
    Blur

---
+++ Assets/Scripts/Blur.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Blur : MonoBehaviour {
+
+    public Texture blur;
+    public float alpha;
+
+	// Use this for initialization
+	void Start () {
+        
+    }
+
+
+    // Update is called once per frame
+    void Update () {
+        
+    }
+
+    private void OnGUI()
+    {
+        Color temp;
+        temp = GUI.color;
+        temp.a = alpha;
+        GUI.color = temp;
+        GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), blur);
+    }
+
+}
