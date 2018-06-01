0b82023c9672ee4f725a6339997592c4d8cec2a7
miestl <loic.miest@epitech.eu>
2018-06-01 17:50:32 +0900

    Some Modif on UI

---
+++ Assets/Scripts/RespawnOBJ.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RespawnOBJ : MonoBehaviour
+{
+
+	public GameObject[] OBJ_ToRespawn;
+	private GameObject[] OBJSave;
+	
+	// Use this for initialization
+	void Start () {
+		
+		OBJSave = new GameObject[OBJ_ToRespawn.Length];
+		
+		for (int i = 0; i < OBJ_ToRespawn.Length; i++)
+			OBJSave[i] = OBJ_ToRespawn[i];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		for (int i = 0; i < OBJ_ToRespawn.Length; i++)
+		{
+			//if (OBJ_ToRespawn[i] == null)
+				
+		}
+		            
+	}
+}
