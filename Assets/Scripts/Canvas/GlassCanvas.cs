930cc040ef799e4f305c8039f490ee2aabee7a89
Fernand <fernand.veyrier@epitech.eu>
2018-06-01 08:19:51 +0200

    Modified canvas for glass

--- Assets/Scripts/Canvas/GlassCanvas.cs
+++ Assets/Scripts/Canvas/GlassCanvas.cs
@@ -4,10 +4,18 @@ using UnityEngine.UI;
 
 namespace Assets.Scripts.Canvas
 {
+    [System.Serializable]
+    public class AlcoholInfo
+    {
+        public GameObject RootObj;
+        public Text TextAlcohol;
+        public Text TextAlcoholPercentage;
+    }
+
     public class GlassCanvas : AlcoholCanvas
     {
         [SerializeField] private Text _powerUpText;
-        [SerializeField] private Text[] _alcoholText;
+        [SerializeField] private AlcoholInfo[] _alcoholObj;
         [SerializeField] private GlassThrowable _glass;
 
         override protected void Update()
@@ -15,16 +23,17 @@ namespace Assets.Scripts.Canvas
             base.Update();
             _powerUpText.text = _glass.GetEffectName();
             var itemList = _glass.GetLiquidList();
-            for (int i = 0; i < _alcoholText.Length; ++i)
+            for (int i = 0; i < _alcoholObj.Length; ++i)
             {
                 if (i < itemList.Count)
                 {
-                    _alcoholText[i].gameObject.SetActive(true);
-                    _alcoholText[i].text = itemList[i].Percentage.ToString() + " " + itemList[i].Liquid.ToString();
+                    _alcoholObj[i].RootObj.SetActive(true);
+                    _alcoholObj[i].TextAlcohol.text = itemList[i].Liquid.ToString();
+                    _alcoholObj[i].TextAlcoholPercentage.text = itemList[i].Percentage.ToString("0.") + "%";
                 }
                 else
                 {
-                    _alcoholText[i].gameObject.SetActive(false);
+                    _alcoholObj[i].RootObj.SetActive(false);
                 }
             }
         }
